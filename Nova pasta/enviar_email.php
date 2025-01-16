36c79136f8a87be5705c677f35c060724a759383
cannasphere <dev.cannasphere@gmail.com>
2025-01-16 02:54:10 -0300

    Add files via upload

---
+++ Nova pasta/enviar_email.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = htmlspecialchars($_POST['nome']);
+    $email = htmlspecialchars($_POST['email']);
+    $mensagem = htmlspecialchars($_POST['mensagem']);
+
+    $to = "seuemail@dominio.com"; // Substitua pelo seu e-mail
+    $subject = "Nova mensagem do site CannaSphere";
+    $body = "Nome: $nome\nE-mail: $email\n\nMensagem:\n$mensagem";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Erro ao enviar a mensagem. Tente novamente.";
+    }
+}
+?>
